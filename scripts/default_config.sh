04e4970905bdeff8c35de367dd4a80b39f80e499
Ethem Beldagli <198623801+ethembeldagli@users.noreply.github.com>
2026-08-14 08:41:52 +0300

    Update default_config.sh

--- scripts/default_config.sh
+++ scripts/default_config.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
 
-# This script provides common customization options for the ISO
-# 
-# Usage: Copy this file to config.sh and make changes there.  Keep this file (default_config.sh) as-is
-#   so that subsequent changes can be easily merged from upstream.  Keep all customiations in config.sh
+# Configuration file version
+export CONFIG_FILE_VERSION="0.4"
 
-# The version of Ubuntu to generate.  Successfully tested LTS: bionic, focal, jammy, noble
-# See https://wiki.ubuntu.com/DevelopmentCodeNames for details
-export TARGET_UBUNTU_VERSION="focal"
+# Target OS Name
+export TARGET_NAME="lampuntu"
 
-# The Ubuntu Mirror URL. It's better to change for faster download.
-# More mirrors see: https://launchpad.net/ubuntu/+archivemirrors
+# Target Ubuntu version (24.04 LTS Noble Numbat)
+export TARGET_UBUNTU_VERSION="noble"
+
+# Ubuntu Mirror
 export TARGET_UBUNTU_MIRROR="http://us.archive.ubuntu.com/ubuntu/"
 
-# The packaged version of the Linux kernel to install on target image.
-# See https://wiki.ubuntu.com/Kernel/LTSEnablementStack for details
+# Linux Kernel Package
 export TARGET_KERNEL_PACKAGE="linux-generic"
 
-# The file (no extension) of the ISO containing the generated disk image,
-# the volume id, and the hostname of the live environment are set from this name.
-export TARGET_NAME="ubuntu-from-scratch"
+# GRUB boot menu labels
+export GRUB_LIVEBOOT_LABEL="Try Lampuntu 24.04 LTS without installing"
+export GRUB_INSTALL_LABEL="Install Lampuntu 24.04 LTS"
 
-# The text label shown in GRUB for booting into the live environment
-export GRUB_LIVEBOOT_LABEL="Try Ubuntu FS without installing"
-
-# The text label shown in GRUB for starting installation
-export GRUB_INSTALL_LABEL="Install Ubuntu FS"
-
-# Packages to be removed from the target system after installation completes succesfully
+# Packages to remove after installation
 export TARGET_PACKAGE_REMOVE="
     ubiquity \
     casper \
@@ -36,26 +28,29 @@ export TARGET_PACKAGE_REMOVE="
     os-prober \
 "
 
-# Package customisation function.  Update this function to customize packages
-# present on the installed system.
+# Package customization function
 function customize_image() {
-    # install graphics and desktop
+    # Install graphics and minimal desktop
     apt-get install -y \
         plymouth-themes \
-        ubuntu-gnome-desktop \
-        ubuntu-gnome-wallpapers
+        ubuntu-desktop-minimal \
+        ubuntu-wallpapers
 
-    # useful tools
+    # Install necessary tools
     apt-get install -y \
-        clamav-daemon \
-        terminator \
         apt-transport-https \
         curl \
+        wget \
         vim \
         nano \
         less
 
-    # purge
+    # Execute custom Lampuntu rebrand script inside chroot
+    if [ -f "/root/make-lampuntu.sh" ]; then
+        bash /root/make-lampuntu.sh
+    fi
+
+    # Purge unused default applications
     apt-get purge -y \
         transmission-gtk \
         transmission-common \
@@ -65,7 +60,3 @@ function customize_image() {
         aisleriot \
         hitori
 }
-
-# Used to version the configuration.  If breaking changes occur, manual
-# updates to this file from the default may be necessary.
-export CONFIG_FILE_VERSION="0.4"
